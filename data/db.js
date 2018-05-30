5fb2aadf396623e25bf197356fb476dad6efeab0
Cole Phillips <nphillips78@gmail.com>
2018-05-30 16:44:33 -0400

    packages installed, comments in database files

--- data/db.js
+++ data/db.js
@@ -13,17 +13,19 @@ module.exports = {
 function find() {
   return db('posts');
 }
+//just looks and distributes the table as an array
 
 function findById(id) {
   return db('posts').where({ id: Number(id) });
 }
-
+// takes the post table and says where the id is in the table, you pass it a number id in req.params to filter it out
 function insert(post) {
   return db('posts')
     .insert(post)
     .then(ids => ({ id: ids[0] }));
 }
-
+// select the table you want then used insert based on the parameter you feed it
+// then let the ids be sav ed in the array at the index of id: ids[0] from the post  its incrememented automatically
 function update(id, post) {
   return db('posts')
     .where('id', Number(id))
--- data/migrations/20180401213248_add_posts_table.js
+++ data/migrations/20180401213248_add_posts_table.js
@@ -1,11 +1,12 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex, Promise) { // this is our then?
   console.log('creating posts table');
+  // takes a knex object to create
 
   return knex.schema.createTable('posts', function(posts) {
-    posts.increments();
+    posts.increments(); // adds an id to the posts, incrementing numbers
 
-    posts.string('title', 255).notNullable();
-    posts.text('contents').notNullable();
+    posts.string('title', 255).notNullable(); // what type, how many characters, can't be blank
+    posts.text('contents').notNullable(); // text type has unlimited characters, can't be blank
 
     posts.timestamps(true, true);
 
@@ -15,4 +16,4 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists('posts');
-};
+}; // this is our catch
